9929ef4541b5461fbf3e0790d6715cd3db5f8dc5
mooky1007 <mooky1007@naver.com>
2023-07-26 12:21:31 +0900

    update simple action

--- merge.js
+++ merge.js
@@ -8,35 +8,124 @@ class Board {
 
     init() {
         this.blocks.forEach((_, idx) => {
-            this.blocks[idx] = new Block();
+            this.blocks[idx] = new Block(idx);
         });
 
-        this.render();
-    }
-
-    render() {
         this.el.innerHTML = '';
         this.blocks.forEach(block => {
             this.el.appendChild(block.el);
         });
+
+        this.customLevelSetting();
+    }
+
+    render() {
+        this.blocks.forEach(block => {
+            block.render();
+        });
+    }
+
+    createRandomBlock() {
+        const emptyBlocks = this.blocks.filter(block => block.data.level === null);
+        const randomBlock = emptyBlocks[Math.floor(Math.random() * emptyBlocks.length)];
+        randomBlock.data.level = 2;
+        randomBlock.render();
+    }
+
+    customLevelSetting() {
+        this.blocks[0].setLevel(1);
+        this.blocks[4].setLevel(1);
+        this.blocks[5].setLevel(1);
+        this.blocks[15].setLevel(1);
+        this.blocks[16].setLevel(1);
+        this.blocks[18].setLevel(1);
+        this.blocks[21].setLevel(1);
+        this.blocks[26].setLevel(1);
+        this.blocks[30].setLevel(1);
+        this.blocks[55].setLevel(1);
+        this.blocks[78].setLevel(1);
+        this.blocks[99].setLevel(1);
+        this.blocks[17].setLevel(1);
+        this.blocks[19].setLevel(1);
+        this.blocks[77].setLevel(1);
+        this.blocks[87].setLevel(1);
+        this.blocks[78].setLevel(1);
+        this.blocks[91].setLevel(1);
+        this.render();
     }
 }
 
 class Block {
-    constructor() {
+    constructor(id) {
+        this.id = id;
         this.el = null;
         this.clicked = false;
+        this.data = {
+            type: 0,
+            level: null
+        }
         this.create();
     }
 
     create() {
         const div = document.createElement('div');
         div.classList.add('block');
-
-        div.addEventListener('mousedown', this.mouseDown.bind(this));
-        document.addEventListener('mouseup', this.mouseUp.bind(this));
-
+        div.id = this.id;
+        div.style.color = "#fff";
+        div.draggable = true;
         this.el = div;
+
+        div.addEventListener('dragenter', e => {
+            document.querySelectorAll('.drag-on').forEach(el => {
+                el.classList.remove('drag-on');
+            });
+            div.classList.add('drag-on');
+        })
+
+        div.addEventListener('dragend', e => {
+            if(!document.querySelector('.drag-on')) return;
+
+            const fromEl = this.el;
+            const toEl = document.querySelector('.drag-on');
+
+            const temp = fromEl.innerHTML;
+
+            if(toEl.innerHTML !== ""){
+                if(toEl.innerHTML === fromEl.innerHTML){
+                    fromEl.innerHTML = '';
+                    toEl.innerHTML = +toEl.innerHTML + +temp;
+                }else{
+                    fromEl.innerHTML = toEl.innerHTML;
+                    toEl.innerHTML = temp;    
+                }
+            }else{
+                fromEl.innerHTML = toEl.innerHTML;
+                toEl.innerHTML = temp;
+            }
+
+            toEl.classList.remove('drag-on');
+        })
+
+        div.addEventListener('touchstart', e => {
+            console.log(div.id, 'touchstart')
+        })
+        div.addEventListener('touchmove', e => {
+            console.log(div.id, 'touchmove')
+        })
+        div.addEventListener('touchend', e => {
+            console.log(div.id, 'touchend')
+        })
+        div.addEventListener('touchcancel', e => {
+            console.log(div.id, 'touchcancel')
+        })
+    }
+
+    setLevel(level) {
+        this.data.level = level;
+    }
+
+    render() {
+        this.el.innerHTML = this.data.level;
     }
 
     createClone() {
@@ -57,18 +146,5 @@ class Block {
         });
 
         return clone;
-    }
-
-    mouseDown() {
-        this.clicked = true;
-        document.body.appendChild(this.createClone());
-        this.el.classList.add('active');
-    }
-
-    mouseUp() {
-        if(this.clicked) return;
-        this.clicked = false;
-        this.clone.remove();
-        this.el.classList.remove('active');
     }
 }
